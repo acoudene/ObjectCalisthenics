05cc87280d32d8db1be77fba90ec61259f66284d
Anthony COUDENE <anthony.coudene@technidata-web.com>
2024-04-07 19:13:01 +0200

    Add a comment on sharplab.io to disassembly

--- NineCalisthenicRules/_9_TellDontAsk_NoGetterSetter/ShowRoomSeller.cs
+++ NineCalisthenicRules/_9_TellDontAsk_NoGetterSetter/ShowRoomSeller.cs
@@ -3,7 +3,9 @@
 public class ShowRoomSeller
 {
   private readonly ReservationStock _reservationStock = new ReservationStock();
-  
+
+  // Here we can tolerate get as public and always ask ourselves for set instead of more business method
+  // See also disassembly here: https://sharplab.io/
   public int Capital { get; private set; } = 0;
 
   public void BookASpectacle(string bookingName)
